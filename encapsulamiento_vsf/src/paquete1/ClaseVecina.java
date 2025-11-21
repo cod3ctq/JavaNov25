875f23b8137abec30f0789df6bd437fa9a2d6ad5
GVladimirSF <gvladimir.sanchez@hotmail.com>
2025-11-21 10:03:32 -0600

    practica de clase

---
+++ encapsulamiento_vsf/src/paquete1/ClaseVecina.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class ClaseVecina {
+
+    ClaseConceptual con = new ClaseConceptual();
+
+    public ClaseVecina(){
+
+
+        con.accionCompartida();//esta es la invocacion a un metodo protegido
+    }
+}
